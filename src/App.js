94a157ce4fc620c99a624e9efdb112ec5fcfc7cf
Ezabulbary <ezabul.bary@gmail.com>
2022-04-21 15:43:21 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Meal from './components/Meal/Meal';
 import Restaurant from './components/Restaurant/Restaurant';
 
 function App() {
   return (
     <div className="App">
       <Restaurant></Restaurant>
+      <Meal></Meal>
     </div>
   );
 }
